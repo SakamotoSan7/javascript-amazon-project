122d4df0bada7eb703a788be9cb787e899848062
Kristianto <inikristiantoitu@gmail.com>
2023-12-03 23:16:57 +0700

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,10 +74,18 @@ document.querySelectorAll('.js-add-to-cart-button')
                 matchingItem.quantity += 1;
             } else {
                 cart.push({
-                    name : productId
+                    name : productId,
                     quantity : 1
                 });
             };
 
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity
+            });
+
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
         })
     })
