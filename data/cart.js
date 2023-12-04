0e9532b2ab2a26971cd87401528a5f5cdffc2a15
Kristianto <inikristiantoitu@gmail.com>
2023-12-04 20:52:42 +0700

    generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+},{
+    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1
+}];
 
 export function addToCart(productId){
     let matchingcartItem;
@@ -13,7 +19,7 @@ export function addToCart(productId){
         matchingcartItem.quantity += 1;
     } else {
         cart.push({
-            name : productId,
+            productId : productId,
             quantity : 1
         });
     };
