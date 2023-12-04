6ea1573dce9efcc3932768b357f77bf2438e90fc
Kristianto <inikristiantoitu@gmail.com>
2023-12-04 18:18:25 +0700

    move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let matchingcartItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId){
+            matchingcartItem = cartItem
+        };
+    });
+
+    if (matchingcartItem) {
+        matchingcartItem.quantity += 1;
+    } else {
+        cart.push({
+            name : productId,
+            quantity : 1
+        });
+    };
+};
