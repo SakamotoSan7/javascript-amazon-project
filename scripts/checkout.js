73eb450e620efcbf7ce1d725e7d23d0dfa2ad9b9
Kristianto <inikristiantoitu@gmail.com>
2024-06-20 17:55:34 +0700

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCartQuantity, renderItemsQuantity } from '../data/cart.js';
 //import '../data/cart-oop.js';
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 renderPaymentSummary();
